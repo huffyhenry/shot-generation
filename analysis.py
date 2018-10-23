03463f1bc61c8e89a488dd8b648a044c5a3a0d0f
Marek Kwiatkowski <marek@mareklab.org>
2018-10-23 20:51:14 +0200

    Polish graphs

--- analysis.py
+++ analysis.py
@@ -1,8 +1,15 @@
 """ Code to help with analysis of fitted models. """
 from adjustText import adjust_text
+import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 import scipy.special as ss
+
+plt.style.use('Solarize_Light2')
+matplotlib.rcParams['axes.labelsize'] = 9
+matplotlib.rcParams['xtick.labelsize'] = 8
+matplotlib.rcParams['ytick.labelsize'] = 8
+matplotlib.rcParams['font.family'] = "Merriweather"
 
 
 def AIC(samples):
@@ -31,56 +38,75 @@ def hdi(vector, width=0.5):
     return x[left], x[right]
 
 
-def gen_scatter(samples, team_map, cis=True):
-    """ Quick visualisation of the team coefficient samples. """
+def team_profiles(gen_samples, conv_samples, teammap):
+    """
+    Visualise the fit of the generation and coversion models as a pair
+    of scatter plots.
+    """
     def weibull_means(shape, scales):
         """ Return the mean of the Weibull distro, vectorized over scale. """
         return ss.gamma(1.0 + 1.0/shape)*scales
 
-    plt.style.use('Solarize_Light2')
+    generation = gen_samples['generation']
+    prevention = gen_samples['prevention']
+    conversion = conv_samples['conversion']
+    obstruction = conv_samples['obstruction']
+    k = gen_samples['k']  # The Weibull exponent
+    ls = list(teammap.keys())  # Team labels, same for both models
 
-    # Extract the samples
-    generation, prevention = samples['generation'], samples['prevention']
-    k = np.mean(samples['k'])
+    plt.figure(figsize=(10, 6), tight_layout=True)
 
-    labels = list(team_map.keys())  # Magically in the correct order
+    # Attack
+    xs = np.apply_along_axis(np.mean, 0, weibull_means(np.mean(k), 1.0/np.exp(generation)))
+    ys = np.apply_along_axis(np.mean, 0, ss.expit(conversion))
+    ax = plt.subplot(121)
+    ax.scatter(xs, ys)
+    ax.set_xlabel("Expected minutes to next shot taken at 0:0 vs avg opposition")
+    ax.set_ylabel("Expected conversion of a shot taken at 0:0 vs avg opposition")
+    gen_labels = [ax.text(xs[i], ys[i], ls[i]) for i in range(len(ls))]
+    adjust_text(gen_labels, arrowprops={'arrowstyle': '-', 'color': 'gray'})
 
-    # Select quantities to plot and make them interpretable
-    # Nb. mean(prevention) = 0
-    xsamples = weibull_means(k, 1.0/np.exp(generation))
-    ysamples = weibull_means(k, 1.0/np.exp(prevention + np.mean(generation)))
-    xlabel = "Expected time to next shot taken at 0:0 vs avg opposition"
-    ylabel = "Expected time to next shot conceded at 0:0 to avg opposition"
+    # Shot conversion
+    xs = weibull_means(np.mean(k), 1.0 / np.exp(prevention + np.mean(generation)))
+    xs = np.apply_along_axis(np.mean, 0, xs)
+    ys = np.apply_along_axis(np.mean, 0, ss.expit(obstruction + np.mean(conversion)))
 
-    xmeans = np.apply_along_axis(np.mean, 0, xsamples)
-    ymeans = np.apply_along_axis(np.mean, 0, ysamples)
+    ax = plt.subplot(122)
+    ax.scatter(xs, ys)
+    ax.set_xlabel("Expected minutes to next shot conceded at 0:0 to avg opposition")
+    ax.set_ylabel("Expected conversion of a shot taken at 0:0 by avg opposition")
+    ax.yaxis.tick_right()
+    ax.yaxis.set_label_position("right")
+    conv_labels = [ax.text(xs[i], ys[i], ls[i]) for i in range(len(ls))]
+    adjust_text(conv_labels, arrowprops={'arrowstyle': '-', 'color': 'gray'})
 
-    # Plot
-    plt.figure(figsize=(8, 8), tight_layout=True)
-    ax = plt.subplot(111)
-    if cis:
-        xerr = np.apply_along_axis(hdi, 0, xsamples)
-        yerr = np.apply_along_axis(hdi, 0, ysamples)
-        ax.errorbar(xmeans, ymeans, xerr=xerr, yerr=yerr, elinewidth=0.25, fmt='.')
-    else:
-        ax.scatter(xmeans, ymeans)
-
-    labels = [ax.text(xmeans[i], ymeans[i], labels[i], size=6, color='gray')
-              for i in range(len(labels))]
-    adjust_text(labels, arrowprops={'arrowstyle': '-', 'color': 'gray'})
-    ax.set_xlabel(xlabel)
-    ax.set_ylabel(ylabel)
     plt.show()
 
 
-def score_matrix(samples, max_goals=2):
-    m = np.zeros((max_goals + 1, max_goals + 1))
+def score_bars(samples, figsize=(6, 3)):
+    # Hardcode the score order for simplicity
+    # The last estimate, 'other D', ommitted due to large CIs.
+    scores = ['0:1', '0:2', '1:0', '1:1', '1:2', '2:0', '2:1', '2:2',
+              'other W', 'other L']
+    summaries = []
 
-    for i in range(max_goals + 1):
-        for j in range(max_goals + 1):
-                m[i, j] = np.mean(samples['score'][:, i, j])
+    plt.figure(tight_layout=True, figsize=figsize)
+    ax = plt.subplot()
 
-    plt.figure()
-    ax = plt.subplot(111)
-    ax.imshow(m.T, origin='upper')
+    for idx, score in enumerate(scores):
+        draws = np.exp(samples['score_raw'][:, idx])
+        left, right = hdi(draws, width=0.95)
+        mean = np.mean(draws)
+        summaries.append((score, mean, abs(left-mean), abs(right-mean)))
+
+    summaries.sort(key=lambda x: x[1])
+    scores, means, left, right = zip(*summaries)
+
+    ax.axvline(1, linestyle=':', alpha=0.40)
+    ax.errorbar(means, range(len(scores)), xerr=[left, right], fmt=".")
+    ax.set_xlabel("Shooting rate multiplier relative to 0:0 (with 95% CIs)")
+    ax.set_ylabel("Current score")
+    ax.set_yticks(range(len(scores)))
+    ax.set_yticklabels(scores)
+
     plt.show()
